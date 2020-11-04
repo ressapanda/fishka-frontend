a23ffe85bd66767e50b0dc5a0f5373077991f690
Maciej Syguła <mac.sygula@gmail.com>
2020-11-04 19:40:23 +0100

    Disable swipe event on card carousel, remove pytanie from question difficulty and change umiarkowane to srednie, change card width on carousel, get random card in page header, add stepper for stats

--- src/app/modules/home/home/home-statistics/home-statistics.component.ts
+++ src/app/modules/home/home/home-statistics/home-statistics.component.ts
@@ -9,7 +9,24 @@ import { HomeService, IStatistics } from '../../home.service';
 export class HomeStatisticsComponent {
   public statisticsData: IStatistics;
 
+  public statisticsStepper = {
+    questions_count: [5, 10, 25, 50, 100, 200, 250, 500, 1000],
+    categories_count: [5, 10, 15, 30, 50, 80, 100],
+  };
+
   constructor(public homeService: HomeService) {
-    this.homeService.getStatisticsData().subscribe((statistics: IStatistics) => (this.statisticsData = statistics));
+    this.homeService.getStatisticsData().subscribe((statistics: IStatistics) => {
+      this.statisticsData = {
+        questions_count: this.findFirstLess(statistics.questions_count, this.statisticsStepper.questions_count),
+        categories_count: this.findFirstLess(statistics.categories_count, this.statisticsStepper.categories_count),
+      };
+    });
+  }
+
+  findFirstLess(value: number, array: number[]) {
+    return array
+      .sort((a, b) => a - b)
+      .filter((item) => item < value)
+      .reverse()[0];
   }
 }
